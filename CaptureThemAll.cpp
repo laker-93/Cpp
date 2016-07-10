db6c8c6b0786a647c4751846ab96b95ddfe189e0
Luke Purnell <luke_purnell@hotmail.com>
2016-07-10 22:37:15 +0100

    First version of capture them all

---
+++ CaptureThemAll.cpp
@@ -0,0 +1,79 @@
+
+// Program to print BFS traversal from a given source vertex. BFS(int s) 
+// traverses vertices reachable from s.
+#include<iostream>
+#include <list>
+ 
+using namespace std;
+ 
+struct cell {
+    char column;
+    int row;
+}
+// This class represents a directed graph using adjacency list representation
+class CaptureThemAll
+{
+    public:
+        int fastKnight(string king, string rook, string queen);
+        cell parse_str(string piece);
+        int BFS(cell start, cell dst);  // returns distance from start to dst
+};
+ 
+/*
+ * cells available to knight at (x, i) are S(x,i) := 
+ * (x +/- 2, i +/- 1) U (x +/- 1, i +/- 2)
+ * from S(x,i) calculate minimum of (k -> r) and (k -> q) then add (r -> q)
+ */
+int CaptureThemAll::fastKnight(string king, string rook, string queen)
+{
+    cell k = parse_str(king);
+    cell r = parse_str(rook);
+    cell q = parse_str(queen);
+    int min_r = BFS(k, r);
+    int min_q = BFS(k, q);
+    int min_rq = BFS(r, q);
+    return min(min_r, min_q) + min_rq;
+}
+void CaptureThemAll::BFS(cell start, cell dst)
+{
+    // Mark all the vertices as not visited
+    // V = 8;
+    bool *visited = new bool[V][V];
+    for(int i = 0; i < V; i++)
+        for(int j = 0; j < V; j++)
+            visited[i][j] = false;
+ 
+    // Create a queue for BFS
+    list<cell> queue;
+ 
+    // struct cell s = parse_knight(knight);
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+ 
+ 
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+ 
+        // Get all adjacent vertices of the dequeued vertex s
+        // If a adjacent has not been visited, then mark it visited
+        // and enqueue it
+        for(i = 0; i < 8; i++)
+        {
+            next_cell = cell(s.row + 2, s.column + 1);
+            //...
+
+            if(!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
+ 
+ 
